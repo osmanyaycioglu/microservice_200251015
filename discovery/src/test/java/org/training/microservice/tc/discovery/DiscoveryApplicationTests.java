e11d8f53f1a65d2a1ab7f7b8687f660c185be4d0
osman yaycioglu <osman.yaycioglu@gmail.com>
2025-10-16 09:59:34 +0300

    2

---
+++ discovery/src/test/java/org/training/microservice/tc/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.microservice.tc.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ms-order/src/test/java/org/training/microservice/tc/msorder/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.microservice.tc.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
